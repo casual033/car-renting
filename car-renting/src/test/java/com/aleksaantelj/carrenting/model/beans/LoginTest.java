4ec14d2643a8e3f7820aa758e568edb69cab06be
Aleksa Antelj <Aleksa Antelj@AleksaAntelj-PC>
2011-04-14 14:01:24 +0200

    Added tests for Login page..

---
+++ car-renting/src/test/java/com/aleksaantelj/carrenting/model/beans/LoginTest.java
@@ -0,0 +1,69 @@
+package com.aleksaantelj.carrenting.model.beans;
+
+import org.apache.tapestry5.test.SeleniumTestCase;
+import org.testng.annotations.Test;
+
+public class LoginTest extends SeleniumTestCase {
+	
+  @Test
+  public void checkPageLoad() {
+	  open(getBaseURL());
+	  waitForPageToLoad();
+	  check("username");
+	  check("password");
+	  check("submit");
+  }
+  
+  @Test(dependsOnMethods="checkPageLoad")
+  public void checkLoginEmpty() {
+	  open(getBaseURL());
+	  waitForPageToLoad();
+	  click("submit");
+	  //assertTextPresent("You must provide value for username");
+	  //assertTextPresent("You must provide value for password");
+	  
+	  check("username");
+	  check("password");
+	  check("submit");
+  }
+  
+  @Test(dependsOnMethods="checkLoginEmpty")
+  public void checkLoginShort() {
+	  open(getBaseURL());
+	  waitForPageToLoad();
+	  type("username","a");
+	  type("password","a");
+	  click("submit");
+	  //assertTextPresent("You must provide at least 5 characters for username");
+	  //assertTextPresent("You must provide at least 5 characters for password");
+	  
+	  check("username");
+	  check("password");
+	  check("submit");
+  }
+  
+  @Test(dependsOnMethods="checkPageLoad")
+  public void checkLoginWrongCredentials() {
+	  open(getBaseURL());
+	  waitForPageToLoad();
+	  type("username","aaaaaa");
+	  type("password","aaaaaa");
+	  click("submit");
+	  waitForPageToLoad();
+	  check("username");
+	  check("password");
+	  check("submit");
+  }
+  
+  @Test(dependsOnMethods="checkLoginWrongCredentials")
+  public void checkLoginSuccess() {
+	  open(getBaseURL());
+	  waitForPageToLoad();
+	  type("username","customer");
+	  type("password","customer");
+	  click("submit");
+	  waitForPageToLoad();
+	  assertTextPresent("Welcome");
+  }
+  
+}
